2034536bdcf73b090806f294d4dfc73a1103d3c5
Nikolai Nanev <niki4etooo@gmail.com>
2025-10-14 10:23:47 +0300

    playing with screen boundries and continuous input

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -12,11 +13,8 @@
 float x_pos = 0.0f;
 float y_pos = 0.0f;
 
-float x_factor = 1.03f;
-float y_factor = 1.03f;
-
-uint16_t x_iterations = 0;
-uint16_t y_iterations = 0;
+float width_rect = 50.0f;
+float height_rect = 50.0f;
 
 int main(int argc, char *argv[])
 {
@@ -25,7 +23,7 @@ int main(int argc, char *argv[])
         "Ship Builder",
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
-        0 // plain window for the SDL renderer
+        SDL_WINDOW_FULLSCREEN // plain window for the SDL renderer
     );
 
     if (SDL_Init(SDL_INIT_VIDEO) != true)
@@ -52,6 +50,29 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    int width = 0, height = 0;
+    int num_displays;
+    SDL_DisplayID *displays = SDL_GetDisplays(&num_displays);
+    SDL_Log("Found %d display(s)", num_displays);
+
+    const SDL_DisplayMode *dm = SDL_GetCurrentDisplayMode(*displays);
+
+    if (dm == nullptr)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get the current display mode: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_free(displays);
+    width = dm->w;
+    height = dm->h;
+
+    std::map<SDL_Keycode, bool> keyMap;
+
+    float speed = 0.5f;
+
     bool done = false;
     while (!done)
     {
@@ -65,42 +86,58 @@ int main(int argc, char *argv[])
 
             if (event.type == SDL_EVENT_KEY_DOWN)
             {
-                SDL_assert(event.type == SDL_EVENT_KEY_DOWN);
-
-                if (event.key.scancode == SDL_SCANCODE_W)
-                {
-                    y_pos -= 1.5f;
-                    y_pos *= x_factor;
-
-                }
-
-                else if (event.key.scancode == SDL_SCANCODE_S)
-                {
-                    y_pos += 1.5f;
-                    y_pos *= y_factor;
-
-                }
-
-                else if (event.key.scancode == SDL_SCANCODE_A)
-                {
-                    x_pos -= 1.5f;
-                    x_pos *= x_factor;
-
-                }
-
-                else if (event.key.scancode == SDL_SCANCODE_D)
-                {
-                    x_pos += 1.5f;
-                    x_pos *= x_factor;
-
-                }
-
-                /* the pressed key was Escape? */
-                if (event.key.key == SDLK_ESCAPE)
-                {
-                    done = true;
-                }
+                keyMap[event.key.key] = true;
             }
+
+            if (event.type == SDL_EVENT_KEY_UP)
+            {
+                keyMap[event.key.key] = false;
+            }
+        }
+
+        if (keyMap[SDLK_S])
+        {
+            y_pos += speed;
+
+            if (y_pos + height_rect >= height)
+            {
+                y_pos -= speed;
+            }
+        }
+
+        if (keyMap[SDLK_W])
+        {
+            y_pos -= speed;
+
+            if (y_pos <= 0)
+            {
+                y_pos += speed;
+            }
+        }
+
+        if (keyMap[SDLK_A])
+        {
+            x_pos -= speed;
+
+            if (x_pos <= 0)
+            {
+                x_pos += speed;
+            }
+        }
+
+        if (keyMap[SDLK_D])
+        {
+            x_pos += speed;
+
+            if (x_pos + width_rect >= width)
+            {
+                x_pos -= speed;
+            }
+        }
+
+        if (keyMap[SDLK_ESCAPE])
+        {
+            done = true;
         }
 
         // Clear screen (black)
@@ -108,7 +145,7 @@ int main(int argc, char *argv[])
         SDL_RenderClear(renderer);
 
         // Draw a red rectangle (use SDL_FRect for SDL3 renderer API)
-        SDL_FRect rectf = {x_pos, y_pos, 50.0f, 50.0f};
+        SDL_FRect rectf = {x_pos, y_pos, width_rect, height_rect};
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rectf);
 
